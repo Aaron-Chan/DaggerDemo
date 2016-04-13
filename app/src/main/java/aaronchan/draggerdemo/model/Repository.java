6ddcc3492e8ea33abe83a5096fb7ce08d6203105
AaronChan <AaronChan>
2016-04-13 16:15:07 +0800

    init package structure

---
+++ app/src/main/java/aaronchan/draggerdemo/model/Repository.java
@@ -0,0 +1,34 @@
+package aaronchan.draggerdemo.model;
+
+/**
+ * Created by Administrator on 2016/4/13.
+ */
+public class Repository {
+    private String name;
+    private String description;
+    private String fullName;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+}
